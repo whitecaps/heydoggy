5e3956bd976866264327b0e4bbcbc806f99ddf0b
whitecaps <white.caps@yahoo.com>
2014-08-02 19:26:43 -0700

    moved root route to top

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  
+  root "events#index"
   get 'signup' => 'users#new'
   get 'signin' => 'sessions#new'
   resource :session
   resources :users
 
-  root "events#index"
+  
   resources :events do
     resources :registrations
   end
