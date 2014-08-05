4122e6932f8712ff57c4abc894642a9183f31565
whitecaps <white.caps@yahoo.com>
2014-08-04 17:22:07 -0700

    removed sleep

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -21,7 +21,6 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 			session[:user_id] = @user.id
-			sleep 5.0
 			redirect_to @user, notice: "Thanks for signing up!"
 		else
 			render :new
