097d373f71c70d08da62c7767d504639f7817698
whitecaps <white.caps@yahoo.com>
2014-08-05 10:58:12 -0700

    put image next to registree list

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -1,2 +1,9 @@
 module RegistrationsHelper
+	def image_for_thumb(user)
+  		if user.image.exists?
+    		image_tag(user.image.url(:thumb))
+  		else
+   			 image_tag('placeholder.jpg')
+  		end
+	end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,4 +6,12 @@ module UsersHelper
    			 image_tag('placeholder.jpg')
   		end
 	end
+
+	def image_for_thumb(user)
+  		if user.image.exists?
+    		image_tag(user.image.url(:thumb))
+  		else
+   			 image_tag('placeholder.jpg')
+  		end
+	end
 end
