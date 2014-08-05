a7fbd58c639cc25be8fb64d5c119c4054904ee0c
whitecaps <white.caps@yahoo.com>
2014-08-05 11:06:15 -0700

    added thumbnail for placeholder image

--- app/helpers/registrations_helper.rb
+++ app/helpers/registrations_helper.rb
@@ -3,7 +3,7 @@ module RegistrationsHelper
   		if user.image.exists?
     		image_tag(user.image.url(:thumb))
   		else
-   			 image_tag('placeholder.jpg')
+   			 image_tag('placeholder_thumb.jpg')
   		end
 	end
 end
